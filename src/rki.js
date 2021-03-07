04818126e08e74a7c26f50cb2e67a79fc64d36ca
Frank Dutenhöfer <frank@dutenhoefer.de>
2021-03-07 15:26:18 +0100

    Added boostrap to the example (just for fun) and made the js more robust, e.g. it checks now for <tbody>

--- src/rki.js
+++ src/rki.js
@@ -3,6 +3,11 @@ const elemID = "rki"
 
 function displayTable() {
    var tbl = document.getElementById(elemID)
+   var tbody = tbl.getElementsByTagName('tbody')[0]
+   if(!tbody) {
+      tbl.createTBody()
+      tbody = tbl.getElementsByTagName('tbody')[0]
+   }
 
    fetch('https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=1%3D1&outFields=county,cases7_per_100k,cases7_per_100k_txt,BL&returnGeometry=false&returnDistinctValues=true&outSR=4326&f=json')
       .then(response => response.json())
@@ -10,7 +15,7 @@ function displayTable() {
          var filteredRkiData = rkiData.features
             .filter(cur => counties.includes(cur.attributes.county))
             .forEach(curItem => {
-               var newRow = tbl.insertRow()
+               var newRow = tbody.insertRow()
                var cell1 = newRow.insertCell()
                var cell2 = newRow.insertCell()
                var cell3 = newRow.insertCell()
